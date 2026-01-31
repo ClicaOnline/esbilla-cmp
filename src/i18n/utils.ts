d9421cc1139e6f0fb5ee7fb64e11272c2aa0c414
jlasolis <jorge@clicaonline.com>
2026-01-31 11:56:00 +0100

    Añadiendo idiomas, mejorando la parte de Comunidad, historia personal y manifiesto técnico

--- src/i18n/utils.ts
+++ src/i18n/utils.ts
@@ -8,6 +8,13 @@ export function getLangFromUrl(url: URL) {
 
 export function useTranslations(lang: keyof typeof ui) {
   return function t(key: keyof typeof ui[typeof defaultLang]) {
+    // 1. Busca na llingua actual (lang)
+    // 2. Si nun esiste (falsy), usa la de defaultLang (asturianu) 🛡️
     return ui[lang][key] || ui[defaultLang][key];
   };
+}
+
+export function getLocalizedPath(url: URL, lang: string) {
+  const [, , ...slug] = url.pathname.split('/');
+  return `/${lang}/${slug.join('/')}`;
 }
